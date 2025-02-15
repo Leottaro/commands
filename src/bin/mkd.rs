3efe7d37328e005e6e87c6b79d934ebd06b18de2
Léo Hafdane <leo.hafdane@gmail.com>
2025-02-15 13:24:56 +0100

    lib.rs Inputs

--- src/bin/mkd.rs
+++ src/bin/mkd.rs
@@ -1,16 +1,22 @@
-use std::{
-    env::{args, set_current_dir},
-    process::exit,
-};
+use std::{env::set_current_dir, path::Path, process::exit};
+
+use zsh_commands::Inputs;
 
 fn main() {
-    let args: Vec<String> = args().skip(1).collect();
-    if args.iter().any(|dir| std::path::Path::new(dir).exists()) {
+    let inputs = Inputs::parse();
+    if inputs.contains_help || inputs.arguments.len() < 2 {
+        println!(
+            "{} <dir1> [dir2] ...\ncreate the dirs (can create foo/bar) and cd into the first one",
+            inputs.name
+        );
+        exit(0);
+    }
+    if inputs.arguments.iter().any(|dir| Path::new(dir).exists()) {
         eprintln!("One or more directories already exist.");
         exit(1);
     }
-    for dir in args.iter() {
+    for dir in inputs.arguments.iter() {
         std::fs::create_dir_all(dir).unwrap();
     }
-    set_current_dir(&args[0]).unwrap();
+    set_current_dir(&inputs.arguments[0]).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,56 @@
+use std::{collections::HashMap, env::args, ffi::OsString, path::PathBuf, process::exit};
 
+pub struct Inputs {
+    pub name: String,
+    pub arguments: Vec<String>,
+    pub options: HashMap<String, String>,
+    pub contains_help: bool,
+}
+
+impl Inputs {
+    pub fn parse() -> Self {
+        let mut args = args();
+
+        let name = args.next().unwrap();
+        let mut arguments = Vec::new();
+        let mut options = HashMap::new();
+        let mut contains_help = false;
+
+        for arg in args.into_iter() {
+            if arg == "--help" || arg == "-h" {
+                contains_help = true;
+                continue;
+            }
+
+            let chars: Vec<char> = arg.chars().collect();
+            if !chars[0].eq(&'-') {
+                arguments.push(arg);
+                continue;
+            }
+
+            let splits: Vec<&str> = arg.trim_matches('-').split('=').collect();
+            if splits.len() != 2 {
+                eprintln!("Error while parsing options: Syntax error");
+                exit(1);
+            }
+            options.insert(splits[0].to_string(), splits[1].to_string());
+        }
+
+        Self {
+            name,
+            arguments,
+            options,
+            contains_help,
+        }
+    }
+}
+
+impl std::fmt::Display for Inputs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Name: {}\nArguments: {:?}\nOptions: {:?}\nContains Help: {}",
+            self.name, self.arguments, self.options, self.contains_help
+        )
+    }
+}
